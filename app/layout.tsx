b3dd4cffc7e8d843ea98a6f42333240678693c8f
amarjotbrar <amarjotbrar0001@gmail.com>
2024-07-21 21:02:24 +0530

    added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 //modules
 import React from 'react';
+import { Metadata } from 'next'
 import classNames from 'classnames/bind';
 
 //components
 import Header from './_components/Header/Header';
 import PageSelector from './_components/PageSelector/PageSelector';
+
+//assets
+import ProfilePhoto from './../public/whitebgprofile.jpg';
 
 //typings
 import { RootLayoutProps } from "./typings";
@@ -13,6 +17,17 @@ import { RootLayoutProps } from "./typings";
 import style from './golobalStyles.module.scss'
 
 const cx = classNames.bind(style);
+
+ 
+export const metadata: Metadata = {
+    title: 'Amarjot',
+    description: "This website showcases my professional journey, skills, and projects. It serves as a platform to highlight my work, including detailed case studies, coding projects, and creative endeavors. Explore my portfolio to see how I turn ideas into reality, solve complex problems, and continuously innovate in the tech world. Whether you're a potential employer, collaborator, or just curious about my work, you'll find a comprehensive overview of my capabilities and accomplishments here.",
+    openGraph: {
+        images: ['./../public/whitebgprofile.jpg'],
+        title: "Amarjot Singh - Portfolio",
+        description: 'This website showcases my professional journey, skills, and projects of Amarjot Singh.'
+    }
+}
 
 const RootLayout = (props: RootLayoutProps) => {
     const { children } = props;
